9ecb95aaf891eae13f92da6c29595b59a816b51d
Samuel Ainsworth <skainsworth@gmail.com>
2018-02-13 21:41:32 -0800

    shit's not bad...

--- app/src/main/java/com/grahamsam/audiomodulator/MainActivity.java
+++ app/src/main/java/com/grahamsam/audiomodulator/MainActivity.java
@@ -5,56 +5,57 @@ import android.media.AudioManager;
 import android.media.AudioRecord;
 import android.media.AudioTrack;
 import android.media.MediaRecorder;
-import android.media.audiofx.AutomaticGainControl;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ShortBuffer;
 import java.nio.charset.Charset;
+import java.util.Arrays;
 
-import static android.media.AudioTrack.*;
+import static android.media.AudioTrack.MODE_STATIC;
+import static android.media.AudioTrack.OnPlaybackPositionUpdateListener;
+import static android.media.AudioTrack.getMaxVolume;
 
 public class MainActivity extends AppCompatActivity {
     private static final int SAMPLE_RATE = 44100;
     private static final int SAMPLES_PER_BLOCK = 500;
-//    private static final int PREAMBLE_SAMPLE_GAP = 0;
-//    private static final int START_GAP = 500;
-//    private static final int NUM_PREAMBLES = 1;
-    private static final int FREQUENCY_0 = 3000;
-    private static final int FREQUENCY_1 = 7000;
-    private static final int MAX_RECV_SAMPLES = SAMPLE_RATE * 5;
+//    private static final int FREQUENCY_0 = 3000;
+    private static final int FREQUENCY_1 = 4000;
+    private static final int RECORDER_BUFFER_SIZE_BYTES = 2 * SAMPLE_RATE;
     private static final int PREAMBLE_SEARCH_LENGTH = SAMPLE_RATE * 3;
 
-    private static float[] FREQUENCY_0_ENVELOPE;
-    private static float[] FREQUENCY_1_ENVELOPE;
+    private static final float LOW_AMPLITUDE = (float) 0.1;
+    private static final float HIGH_AMPLITUDE = 1;
+
+//    private static float[] FREQUENCY_0_ENVELOPE;
+//    private static float[] FREQUENCY_1_ENVELOPE;
 
     private static Charset CHARSET = Charset.forName("ASCII");
 
     private static final boolean[] BARKER13 = {true, true, true, true, true, false, false, true, true, false, true, false, true};
 
-    private AudioRecord recorder;
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+//    private AudioRecord recorder;
+    private AsyncTask<Void, Void, Void> recordingAsyncTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        short[] freq0Env = new short[SAMPLES_PER_BLOCK];
-        short[] freq1Env = new short[SAMPLES_PER_BLOCK];
-        writeQAM(freq0Env, 0, freq0Env.length, 1, 0, FREQUENCY_0);
-        writeQAM(freq1Env, 0, freq1Env.length, 1, 0, FREQUENCY_1);
+//        short[] freq0Env = new short[SAMPLES_PER_BLOCK];
+//        short[] freq1Env = new short[SAMPLES_PER_BLOCK];
+//        writeQAM(freq0Env, 0, freq0Env.length, 1, 0, FREQUENCY_0);
+//        writeQAM(freq1Env, 0, freq1Env.length, 1, 0, FREQUENCY_1);
 
-        FREQUENCY_0_ENVELOPE = shortsToFloats(freq0Env);
-        FREQUENCY_1_ENVELOPE = shortsToFloats(freq1Env);
+//        FREQUENCY_0_ENVELOPE = shortsToFloats(freq0Env);
+//        FREQUENCY_1_ENVELOPE = shortsToFloats(freq1Env);
     }
 
     private void writeQAM(short[] buffer, int start, int length, float i, float q, float f0) {
@@ -69,12 +70,22 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void barker13Preamble(short[] buffer, int start) {
+//    private void barker13PreambleFM(short[] buffer, int start) {
+//        for (int i = 0; i < BARKER13.length; i++) {
+//            if (BARKER13[i]) {
+//                writeQAM(buffer, start + i * SAMPLES_PER_BLOCK, SAMPLES_PER_BLOCK, 1, 0, FREQUENCY_1);
+//            } else {
+//                writeQAM(buffer, start + i * SAMPLES_PER_BLOCK, SAMPLES_PER_BLOCK, 1, 0, FREQUENCY_0);
+//            }
+//        }
+//    }
+
+    private void barker13PreambleAM(short[] buffer, int start) {
         for (int i = 0; i < BARKER13.length; i++) {
             if (BARKER13[i]) {
-                writeQAM(buffer, start + i * SAMPLES_PER_BLOCK, SAMPLES_PER_BLOCK, 1, 0, FREQUENCY_1);
+                writeQAM(buffer, start + i * SAMPLES_PER_BLOCK, SAMPLES_PER_BLOCK, HIGH_AMPLITUDE, 0, FREQUENCY_1);
             } else {
-                writeQAM(buffer, start + i * SAMPLES_PER_BLOCK, SAMPLES_PER_BLOCK, 1, 0, FREQUENCY_0);
+                writeQAM(buffer, start + i * SAMPLES_PER_BLOCK, SAMPLES_PER_BLOCK, LOW_AMPLITUDE, 0, FREQUENCY_1);
             }
         }
     }
@@ -94,9 +105,9 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 8; i++) {
             int bit = (byt >> (7 - i)) & 1;
             if (bit == 1) {
-                writeQAM(buffer, start + i * SAMPLES_PER_BLOCK, SAMPLES_PER_BLOCK, 1, 0, FREQUENCY_1);
+                writeQAM(buffer, start + i * SAMPLES_PER_BLOCK, SAMPLES_PER_BLOCK, HIGH_AMPLITUDE, 0, FREQUENCY_1);
             } else {
-                writeQAM(buffer, start + i * SAMPLES_PER_BLOCK, SAMPLES_PER_BLOCK, (float) 0.1, 0, FREQUENCY_1);
+                writeQAM(buffer, start + i * SAMPLES_PER_BLOCK, SAMPLES_PER_BLOCK, LOW_AMPLITUDE, 0, FREQUENCY_1);
             }
         }
     }
@@ -111,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Write the preamble
         int current_ix = 0;
-        barker13Preamble(buffer, 0);
+        barker13PreambleAM(buffer, 0);
         current_ix += preambleNumSamples;
 
         // Write the number of bytes to follow
@@ -172,17 +183,79 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickReceive(View view) {
-        if (this.recorder == null) {
-            System.out.println("Beginning recording...");
-            this.recorder = new AudioRecord(
-                    MediaRecorder.AudioSource.MIC,
-                    SAMPLE_RATE,
-                    AudioFormat.CHANNEL_IN_MONO,
-                    AudioFormat.ENCODING_PCM_16BIT,
-                    2 * MAX_RECV_SAMPLES);
+        // Only start recording if we are not already.
+        if (recordingAsyncTask == null) {
+            // Clear the previously accumulated recording data.
+            this.outputStream.reset();
 
-//            System.out.println(this.recorder.getState());
-            this.recorder.startRecording();
+            recordingAsyncTask = new AsyncTask<Void, Void, Void>() {
+                private AudioRecord audioRecord;
+
+                @Override
+                protected void onPreExecute() {
+//                mState = State.RECORDING;
+                    audioRecord = new AudioRecord(
+                            MediaRecorder.AudioSource.MIC,
+                            SAMPLE_RATE,
+                            AudioFormat.CHANNEL_IN_MONO,
+                            AudioFormat.ENCODING_PCM_16BIT,
+                            RECORDER_BUFFER_SIZE_BYTES);
+                }
+
+                @Override
+                protected Void doInBackground(Void... params) {
+                    audioRecord.startRecording();
+                    ByteBuffer bb = ByteBuffer.allocate(RECORDER_BUFFER_SIZE_BYTES / 4);
+                    short[] shortBuffer = new short[bb.capacity() / 2];
+                    while (!isCancelled()) {
+                        // We reuse bb so don't fuck it up.
+                        bb.clear();
+
+                        // Returns the number of shorts read, or negative value on error.
+                        int read = audioRecord.read(shortBuffer, 0, shortBuffer.length);
+
+//                        System.out.println(Arrays.toString(shortBuffer));
+
+                        // avoid an infinite loop in the following code
+                        assert read >= 0;
+
+                        for (int i = 0; i < read; i++) {
+                            bb.putShort(shortBuffer[i]);
+                        }
+                        byte[] actualBytes = new byte[read * 2];
+                        bb.rewind();
+                        bb.get(actualBytes, 0, actualBytes.length);
+                        outputStream.write(actualBytes, 0, actualBytes.length);
+
+                        System.out.println("processed buffer read.");
+                    }
+
+                    audioRecord.release();
+                    audioRecord = null;
+
+                    return null;
+                }
+
+                @Override
+                protected void onPostExecute(Void aVoid) {
+                    recordingAsyncTask = null;
+                    if (audioRecord != null) {
+                        audioRecord.release();
+                    }
+                    audioRecord = null;
+                }
+
+                @Override
+                protected void onCancelled() {
+                    recordingAsyncTask = null;
+                    if (audioRecord != null) {
+                        audioRecord.release();
+                    }
+                    audioRecord = null;
+                }
+            };
+
+            recordingAsyncTask.execute();
         }
     }
 
@@ -214,7 +287,7 @@ public class MainActivity extends AppCompatActivity {
 //        return (byte) res;
 //    }
 
-    private byte readSignalByteAM(float[] buffer, int start) {
+    private byte readSignalByteAM(float[] buffer, int start, float threshold) {
         int res = 0;
         for (int j = 0; j < 8; j++) {
             float amplitude = 0;
@@ -224,7 +297,7 @@ public class MainActivity extends AppCompatActivity {
             System.out.print("amplitude: ");
             System.out.println(amplitude);
             res = res << 1;
-            if (amplitude > 50) {
+            if (amplitude > threshold) {
                 res |= 1;
             }
         }
@@ -232,15 +305,103 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickFinish(View view) {
-        if (this.recorder != null) {
-            System.out.println("Finished receiving...");
-            short[] signal = new short[MAX_RECV_SAMPLES];
-            this.recorder.read(signal, 0, signal.length);
-            this.recorder.stop();
-            this.recorder.release();
-            this.recorder = null;
+        if (this.recordingAsyncTask != null) {
+            this.recordingAsyncTask.cancel(true);
+            this.recordingAsyncTask = null;
 
+//            ByteArrayOutputStream _bos = new ByteArrayOutputStream();
+//            byte[] _buffer = {0, (byte) 0xFF};
+//            _bos.write(_buffer, 0, _buffer.length);
+//            Arrays.toString(_bos.toByteArray());
+
+//            byte[] outputStreamByteArray = outputStream.toByteArray();
+//            System.out.println(Arrays.toString(outputStreamByteArray));
+//            short[] signal = new short[outputStreamByteArray.length / 2];
+//            ByteBuffer.wrap(outputStreamByteArray).asShortBuffer().get(signal);
+//            System.out.println(Arrays.toString(signal));
+//            float[] floatSignal = shortsToFloats(signal);
+
+            byte[] outputStreamByteArray = outputStream.toByteArray();
+//            System.out.println(Arrays.toString(outputStreamByteArray));
+            short[] signal = new short[outputStreamByteArray.length / 2];
+            ByteBuffer.wrap(outputStreamByteArray).asShortBuffer().get(signal);
+//            System.out.println(Arrays.toString(signal));
             float[] floatSignal = shortsToFloats(signal);
+
+            // TODO perhaps calculate this ahead of time.
+            short[] preamble = new short[BARKER13.length * SAMPLES_PER_BLOCK];
+            barker13PreambleAM(preamble, 0);
+
+            float[] floatPreamble = shortsToFloats(preamble);
+
+            System.out.println("Searching for preamble");
+            int best_start_ix = 0;
+            float best_correlation = Float.MIN_VALUE;
+            for (int i = 0; i < Math.min(PREAMBLE_SEARCH_LENGTH, floatSignal.length - floatPreamble.length); i++) {
+                float correlation = 0;
+                for (int j = 0; j < preamble.length; j++) {
+                    correlation += floatPreamble[j] * floatSignal[i + j];
+                }
+                if (correlation > best_correlation) {
+                    best_start_ix = i;
+                    best_correlation = correlation;
+                }
+            }
+            System.out.println("... and done.");
+
+            int current_ix = best_start_ix;
+            float sum0Amplitude = 0;
+            float sum1Amplitude = 0;
+            for (int i = 0; i < BARKER13.length; i++) {
+                float amplitude = 0;
+                for (int j = current_ix; j < current_ix + SAMPLES_PER_BLOCK; j++) {
+                    amplitude += Math.abs(floatSignal[j]);
+                }
+                current_ix += SAMPLES_PER_BLOCK;
+
+                if (BARKER13[i]) {
+                    sum1Amplitude += amplitude;
+                } else {
+                    sum0Amplitude += amplitude;
+                }
+            }
+            float avg0Amplitude = sum0Amplitude / 4;
+            float avg1Amplitude = sum1Amplitude / 9;
+            float amplitudeThreshold = (avg0Amplitude + avg1Amplitude) / 2;
+            System.out.print("Amplitude thresholding at ");
+            System.out.println(amplitudeThreshold);
+
+            System.out.print("current_ix before");
+            System.out.println(current_ix);
+
+            current_ix = best_start_ix + floatPreamble.length;
+            System.out.print("current_ix after");
+            System.out.println(current_ix);
+
+            System.out.print("Starting read at ");
+            System.out.println(current_ix);
+            byte firstLengthByte = readSignalByteAM(floatSignal, current_ix, amplitudeThreshold);
+            current_ix += 8 * SAMPLES_PER_BLOCK;
+            byte secondLengthByte = readSignalByteAM(floatSignal, current_ix, amplitudeThreshold);
+            current_ix += 8 * SAMPLES_PER_BLOCK;
+
+            int messageLength = ((firstLengthByte & 0xFF) << 8) | (secondLengthByte & 0xFF);
+            // TODO don't go over the length of the signal thing.
+            System.out.print("Message length: ");
+            System.out.println(messageLength);
+            System.out.println("Demodulating...");
+            byte[] recvBytes = new byte[messageLength];
+            int messageLengthFixed = Math.min(messageLength, (floatSignal.length - current_ix) / (8 * SAMPLES_PER_BLOCK));
+            for (int i = 0; i < messageLengthFixed; i++) {
+                recvBytes[i] = readSignalByteAM(floatSignal, current_ix, amplitudeThreshold);
+                current_ix += 8 * SAMPLES_PER_BLOCK;
+            }
+            System.out.println("... and done");
+
+            String recvMessage = new String(recvBytes, CHARSET);
+            EditText editText = findViewById(R.id.editText2);
+            editText.setText(recvMessage);
+        }
 
 //        try {
 //            File file = File.createTempFile("received", "stuff");
@@ -255,12 +416,6 @@ public class MainActivity extends AppCompatActivity {
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
-
-            short[] preamble = new short[BARKER13.length * SAMPLES_PER_BLOCK];
-            barker13Preamble(preamble, 0);
-
-            float[] floatPreamble = shortsToFloats(preamble);
-
 //        try {
 //            File file = File.createTempFile("truepreamble", "");
 //            OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(file));
@@ -274,44 +429,5 @@ public class MainActivity extends AppCompatActivity {
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
-
-            System.out.println("Searching for preamble");
-            int best_start_ix = 0;
-            float best_correlation = Float.MIN_VALUE;
-            for (int i = 0; i < PREAMBLE_SEARCH_LENGTH; i++) {
-                float correlation = 0;
-                for (int j = 0; j < preamble.length; j++) {
-                    correlation += floatPreamble[j] * floatSignal[i + j];
-                }
-                if (correlation > best_correlation) {
-                    best_start_ix = i;
-                    best_correlation = correlation;
-                }
-            }
-            System.out.println("... and done.");
-
-            int current_ix = best_start_ix + floatPreamble.length;
-            byte firstLengthByte = readSignalByteAM(floatSignal, current_ix);
-            current_ix += 8 * SAMPLES_PER_BLOCK;
-            byte secondLengthByte = readSignalByteAM(floatSignal, current_ix);
-            current_ix += 8 * SAMPLES_PER_BLOCK;
-
-            int messageLength = ((firstLengthByte & 0xFF) << 8) | (secondLengthByte & 0xFF);
-//            messageLength = Math.min(messageLength, 45); // HACK
-            messageLength = 16;
-            System.out.print("Message length: ");
-            System.out.println(messageLength);
-            System.out.println("Demodulating...");
-            byte[] recvBytes = new byte[messageLength];
-            for (int i = 0; i < messageLength; i++) {
-                recvBytes[i] = readSignalByteAM(floatSignal, current_ix);
-                current_ix += 8 * SAMPLES_PER_BLOCK;
-            }
-            System.out.println("... and done");
-
-            String recvMessage = new String(recvBytes, CHARSET);
-            EditText editText = findViewById(R.id.editText2);
-            editText.setText(recvMessage);
-        }
     }
 }
